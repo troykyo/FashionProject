0ae3f27ee6c49603c1aea2f50a12d9f57fcdf9bd
Yves <yvesprinz59@gmail.com>
2023-11-17 15:49:24 +0100

    Fingergun recognition seperated
    gave the fingergun its own bit of code to call upon separately. many other gestures should be made this way
    
    ALSO IT TOOK FOREVER BECAUSE APPAERNTLY SHIT BREAKS IF YOU TRY IT ANYWHERE OTHER THAN "OnUpdatedHands"

--- Assets/Scripts/JointTracking.cs
+++ Assets/Scripts/JointTracking.cs
@@ -13,29 +13,20 @@ public class JointTracking : MonoBehaviour
 
     public GameObject XROrigin;
 
-    //public GameObject handObject;
-    /*private XRHand rightHand;
-    private XRHand leftHand;
-    private XRHandJoint indexTip;
-    private XRHandJoint thumbTip;*/
-    //use these instead. makes the code more readable
-
     Vector3[] leftJointPositions;
     Vector3[] rightJointPositions;
     Vector3[] leftJointRotations;
     Vector3[] rightJointRotations;
 
     public float fingerGunDistanceThreshold;
-    
+
+    private bool soloGesture;
 
     private bool isFingerGun = false;
 
 
     void Start()
     {
-        
-        
-
         var handSubsystems = new List<XRHandSubsystem>();
         SubsystemManager.GetSubsystems(handSubsystems);
 
@@ -93,39 +84,11 @@ public class JointTracking : MonoBehaviour
 
         SubscribeHandSubsystem();
         //HandVisualizer code above
+        //Data on hand positions is gathered above, anything done with that data should be handled below
+        //not quite how it works. just do it all in "OnUpdatedHands"
 
-        /*Debug.Log("trying to find hands...");
-        rightHand = m_HandSubsystem.rightHand;
-        Debug.Log("right hand should now be: " + rightHand);
+        //FingergunCheck(); //Note to self: Possibly split this so both hands can be checked individually if moving is desired during other actions for example
 
-            Debug.Log("trying to get joints...");
-            indexTip = rightHand.GetJoint(XRHandJointID.IndexTip);
-            thumbTip = rightHand.GetJoint(XRHandJointID.ThumbTip);
-            Debug.Log("Index joint should be: " + indexTip + ". Thumb joint should be: " + thumbTip);
-
-        Debug.Log("locking joint positions...");
-        indexTipPosition = ToWorldPose(indexTip, XROrigin.transform).position;
-        thumbTipPosition = ToWorldPose(thumbTip, XROrigin.transform).position;
-        Debug.Log("index and thumb should be at: " + indexTipPosition + ", and " + thumbTipPosition);*/
-
-        //ChatGPT code
-        
-        //Debug.Log();
-
-        //for loop for the list of joints (missing hand refrence(What is "hand" in this context?!?))
-        /*for(var i = XRHandJointID.BeginMarker.ToIndex();
-        i < XRHandJointID.EndMarker.ToIndex();
-        i++)
-{
-            var trackingData = hand.GetJoint(XRHandJointIDUtility.FromIndex(i));
-
-            if (trackingData.TryGetPose(out Pose pose))
-            {
-                // displayTransform is some GameObject's Transform component
-                displayTransform.localPosition = pose.position;
-                displayTransform.localRotation = pose.rotation;
-            }
-        }*/
     }
 
     void UnsubscribeHandSubsystem()
@@ -148,40 +111,6 @@ public class JointTracking : MonoBehaviour
         XRHandSubsystem.UpdateSuccessFlags updateSuccessFlags,
         XRHandSubsystem.UpdateType updateType)
     {
-        //for clarity for both myself and others using this:
-        //"OnUpdatedHands" runs whenever "SubscribeHandSubsystem" runs, which on it's turn is ran every Update.
-
-        //for reference, these are the IDs assigned to each joint:
-        /*Invalid = 0,
-        BeginMarker = 1,
-        Wrist = 1,
-        Palm = 2,
-        ThumbMetacarpal = 3,
-        ThumbProximal = 4,
-        ThumbDistal = 5,
-        ThumbTip = 6,
-        IndexMetacarpal = 7,
-        IndexProximal = 8,
-        IndexIntermediate = 9,
-        IndexDistal = 10,
-        IndexTip = 11,
-        MiddleMetacarpal = 12,
-        MiddleProximal = 13,
-        MiddleIntermediate = 14,
-        MiddleDistal = 15,
-        MiddleTip = 16,
-        RingMetacarpal = 17,
-        RingProximal = 18,
-        RingIntermediate = 19,
-        RingDistal = 20,
-        RingTip = 21,
-        LittleMetacarpal = 22,
-        LittleProximal = 23,
-        LittleIntermediate = 24,
-        LittleDistal = 25,
-        LittleTip = 26,
-        EndMarker = 27
-        */
 
         if (updateType == XRHandSubsystem.UpdateType.Dynamic)
             return;
@@ -193,7 +122,7 @@ public class JointTracking : MonoBehaviour
 
         Vector3[] GetAllJointPositions(string handedness) //Returns the Vectors for every joint on the requested hand. (left or right)
         {
-            Vector3[] jointVectorData = new Vector3[XRHandJointID.EndMarker.ToIndex()+1]; //amount of joints is 27. filling this in like this, in case that ever changes
+            Vector3[] jointVectorData = new Vector3[XRHandJointID.EndMarker.ToIndex()+1];
 
             for (var i = XRHandJointID.BeginMarker.ToIndex();
                  i < XRHandJointID.EndMarker.ToIndex();
@@ -210,7 +139,7 @@ public class JointTracking : MonoBehaviour
 
         Vector3[] GetAllJointRotations(string handedness)
         {
-            Vector3[] jointRotationData = new Vector3[XRHandJointID.EndMarker.ToIndex()+1]; //amount of joints is 27. filling this in like this, in case that ever changes
+            Vector3[] jointRotationData = new Vector3[XRHandJointID.EndMarker.ToIndex()+1];
 
             for (var i = XRHandJointID.BeginMarker.ToIndex();
                  i < XRHandJointID.EndMarker.ToIndex();
@@ -238,7 +167,7 @@ public class JointTracking : MonoBehaviour
                 Debug.LogError("Handedness incorrectly identified! Joint can't be found if your hand is neither left nor right!");
                 return new Vector3();
             }
-        } //returns the postion of the joint in a vector3
+        } //returns the postion of the joint as a vector3
 
         Vector3 GetJointRotation(string handedness, XRHandJointID jointID)
         {
@@ -257,14 +186,45 @@ public class JointTracking : MonoBehaviour
             }
         } //returns the rotation of the joint, as a quaternion converted to an euler angle
 
-        //bool leftHandTracked = subsystem.leftHand.isTracked;
-        //bool rightHandTracked = subsystem.rightHand.isTracked;
+        FingergunCheck();
+    }
 
-        //Debug.Log("LeftThumbtip is at: " + leftJointPositions[6]);
-
+    //for reference, these are the IDs assigned to each joint:
+    /*Invalid = 0
+    BeginMarker = 1
+    Wrist = 1
+    Palm = 2
+    ThumbMetacarpal = 3
+    ThumbProximal = 4
+    ThumbDistal = 5
+    ThumbTip = 6
+    IndexMetacarpal = 7
+    IndexProximal = 8
+    IndexIntermediate = 9
+    IndexDistal = 10
+    IndexTip = 11
+    MiddleMetacarpal = 12
+    MiddleProximal = 13
+    MiddleIntermediate = 14
+    MiddleDistal = 15
+    MiddleTip = 16
+    RingMetacarpal = 17
+    RingProximal = 18
+    RingIntermediate = 19
+    RingDistal = 20
+    RingTip = 21
+    LittleMetacarpal = 22
+    LittleProximal = 23
+    LittleIntermediate = 24
+    LittleDistal = 25
+    LittleTip = 26
+    EndMarker = 27
+    */
+    
+    //This checks if the hands are currently in a "fingergun" position, which would then move the player
+    void FingergunCheck()
+    {
         // Calculate the distance between the thumb and index tips.
-
-        //Debug.Log(leftJointPositions[6]);
 
         float LeftDistance = Vector3.Distance(leftJointPositions[6], leftJointPositions[11]);
         float RightDistance = Vector3.Distance(rightJointPositions[6], rightJointPositions[11]);
@@ -272,47 +232,54 @@ public class JointTracking : MonoBehaviour
         //float leftWristDistance = Vector3.Distance(leftMiddleTipPosition, leftWristPosition);
         //float rightWristDistance = Vector3.Distance(leftMiddleTipPosition, leftWristPosition);
 
-        /*Debug.Log("The index vector3 is: " + leftIndexTipPosition);
-        Debug.Log("The thumb vector3 is: " + leftThumbTipPosition);
-        Debug.Log("The distance between the thumb and the index is: " + LeftDistance);*/
-
         // Check if the distance is below the threshold for a finger gun gesture.
         if ((LeftDistance > fingerGunDistanceThreshold) /*&& (leftWristDistance < fingerGunDistanceThreshold)*/)
         {
             // Finger gun gesture recognized.
             isFingerGun = true;
             Debug.Log("Finger Gun Gesture Recognized!");
+            MovePlayer(false, true);
 
-            //gameObject.transform.position += new Vector3(GameObject.Find("Main Camera").transform.forward.x, 0, GameObject.Find("Main Camera").transform.forward.z)*0.05f;
-            gameObject.transform.position += new Vector3(GameObject.Find("Left Hand").transform.forward.x, 0, GameObject.Find("Left Hand").transform.forward.z).normalized * 0.05f;
-            /*if (!isFingerGun)
-            {
-                
-            }*/
         }
         else
         {
             // Finger gun gesture not recognized.
             //isFingerGun = false;
         }
+
         if ((RightDistance > fingerGunDistanceThreshold) /*&& (rightWristDistance < fingerGunDistanceThreshold)*/)
         {
             // Finger gun gesture recognized.
             isFingerGun = true;
             Debug.Log("Finger Gun Gesture Recognized!");
+            MovePlayer(false, false);
 
-            gameObject.transform.position += new Vector3(GameObject.Find("Main Camera").transform.forward.x, 0, GameObject.Find("Main Camera").transform.forward.z) * 0.05f;
-            //Turn this bit into a seperate function. to avoid more copy/pasting
-            /*if (!isFingerGun)
-            {
-                
-            }*/
         }
         else
         {
             // Finger gun gesture not recognized.
             //isFingerGun = false;
         }
-
     }
+
+    void MovePlayer(bool cameraMove, bool leftHanded)
+    {
+        if (cameraMove)
+        {
+            //Move player based on looking direction
+            gameObject.transform.position += new Vector3(GameObject.Find("Main Camera").transform.forward.x, 0, GameObject.Find("Main Camera").transform.forward.z) * 0.05f;
+        }
+        else
+        {
+            if (leftHanded)
+            {
+                gameObject.transform.position += new Vector3(GameObject.Find("Left Hand").transform.forward.x, 0, GameObject.Find("Left Hand").transform.forward.z).normalized * 0.05f;
+            }
+            else
+            {
+                gameObject.transform.position += new Vector3(GameObject.Find("Left Hand").transform.forward.x, 0, GameObject.Find("Left Hand").transform.forward.z).normalized * 0.05f;
+            }
+        }
+    }
+
 }

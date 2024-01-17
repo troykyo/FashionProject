0fe28b57a1887ad346b48c130b70d0bf0778f88b
Jiminy <jiminy0410@gmail.com>
2024-01-17 17:03:21 +0100

    reset

---
+++ Assets/ResetScr.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.R))
+        {
+            Debug.Log("restet");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}

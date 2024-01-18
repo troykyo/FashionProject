5cc38bffdb988c3625a5a1a35be694870b844972
jiminy <jiminy0410@gmail.com>
2024-01-19 00:35:33 +0100

    sad fix objectengeod

--- Assets/Scripts/PlsDontDistroy.cs
+++ Assets/Scripts/PlsDontDistroy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlsDontKillMe : MonoBehaviour
+public class PlsDontDistroy : MonoBehaviour
 {
     private void Awake()
     {
